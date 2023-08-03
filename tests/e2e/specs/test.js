73d427d290792b3b49a62da0818c631e48695b22
PaolaUribe88 <paolauribe88@gmail.com>
2023-08-02 20:28:31 -0400

    Primer Comentario

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,5 +4,12 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js App')
+  });
+
+  it('Visit about route', ()=>{
+    cy.get('[href="/about"]').click();
+    cy.get('h1').contains('Bienvenidos a la página de about');
   })
+
+
 })
